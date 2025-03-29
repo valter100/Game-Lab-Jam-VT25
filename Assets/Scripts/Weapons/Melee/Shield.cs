1a93bfdeebf0cf0d17bf0050575ac88f5f0c75c2
valter100 <va6648li-s@student.lu.se>
2025-03-29 23:37:58 +0100

    Shield

--- Assets/Scripts/Weapons/Melee/Shield.cs
+++ Assets/Scripts/Weapons/Melee/Shield.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MeleeWeapon
@@ -6,17 +7,48 @@ public class Shield : MeleeWeapon
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        base.Update();
     }
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        if (timeSinceLastAttack > timeBetweenAttacks)
+        {
+            timeSinceLastAttack = 0;
+
+            if (holdingHand.GetHandType() == Hand.HandType.Left)
+            {
+                animator.Play("DefendLeft");
+            }
+            else
+            {
+                animator.Play("DefendRight");
+            }
+
+            currentAmmo--;
+            ammoText.text = currentAmmo.ToString();
+        }
+    }
+
+    public void StartDefense()
+    {
+        isGuarding = true;
+    }
+
+    public void EndDefense()
+    {
+        isGuarding = false;
+
+        if (currentAmmo <= 0)
+        {
+            player.ReturnWeapon(this);
+            currentAmmo = startAmmo;
+        }
     }
 
     protected override void ApplyEquipBonus()
